c6c60085aa8504ddbb09c5b3374e91bd5d1f3298
myusrilh <yusrilhasriansyah@gmail.com>
2020-10-16 21:02:24 +0700

    5 form and its component done tested, except horizontal form

--- cypress/integration/FirstTest.spec.js
+++ cypress/integration/FirstTest.spec.js
@@ -2,23 +2,66 @@
 
 describe('My first test suite',()=>{
 
+    beforeEach('visit url, open form and form layouts',()=>{
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
 
+    })
 
-    it('First Test',()=>{
-        cy.visit('/');
-        cy.contains('Forms').click();
-        cy.contains('Form Layouts').click();
+    it('Inline form test',()=>{
         cy.get('.form-inline').within(()=>{
-            cy.get('input[placeholder = "Jane Doe"]').type('Jane Doe').should('have.value','Jane Doe');
-            cy.get('input[placeholder = "Email"]').type('jane.doe@gmail.com').should('have.value','jane.doe@gmail.com');
-            cy.get('.custom-checkbox').click();
-        });
-        cy.get('#inputEmail1').type('jane.doe@gmail.com').should('have.value','jane.doe@gmail.com');
-        cy.get('#inputPassword2').should('have.attr','type','password');
-        cy.get('#inputPassword2').type('12345').should('have.value','12345');
-        cy.contains('Radios').within(()=>{
-            
-        });
+            cy.get('input[placeholder = "Jane Doe"]').type('Muhammad Yusril Hasriansyah').should('have.value','Muhammad Yusril Hasriansyah')
+            cy.get('input[placeholder = "Email"]').type('yusrilhasriansyah@gmail.com').should('have.value','yusrilhasriansyah@gmail.com')
+            cy.get('.custom-checkbox').click()
+        })
+    })
+
+    it('Using the grid test',()=>{
+        cy.get('#inputEmail1').type('yusrilhasriansyah@gmail.com').should('have.value','yusrilhasriansyah@gmail.com')
+        cy.get('#inputPassword2').should('have.attr','type','password')
+        cy.get('#inputPassword2')
+        .type('113333')
+        .should('have.value','113333')
+        .parents('form')
+        .find('.inner-circle')
+        .first()
+        .click()
+        .parents('form')
+        .contains('Option 2')
+        .parents('nb-radio')
+        .find('.inner-circle')
+        .click()
+        .parents('form')
+        .contains('Disabled Option')
+        .parents('nb-radio')
+        .should('have.attr','disabled')
+    });
+
+    it('Form without label test',()=>{
+        cy.get('input[placeholder = "Recipients"]').type('tuantakur.uhuy@gmail.com').should('have.value','tuantakur.uhuy@gmail.com')
+        cy.get('input[placeholder = "Subject"]').type('Surat Lamaran Kerja').should('have.value','Surat Lamaran Kerja')
+        cy.get('textarea[placeholder = "Message"]').type('Selamat siang, Tuan Takur. Semoga surat ini bisa tembus.').should('contain.value','Selamat siang, Tuan Takur. Semoga surat ini bisa tembus.')
+    });
+
+    it('Basic form test',()=>{
+        cy.get('#exampleInputEmail1')
+        .type('yusrilhasriansyah@gmail.com')
+        .should('have.value','yusrilhasriansyah@gmail.com')
+        cy.get('#exampleInputPassword1').should('have.attr','type','password')
+        cy.get('#exampleInputPassword1')
+        .type('113333')
+        .should('have.value','113333')
+        .parents('form')
+        .find('nb-checkbox')
+        .click()
+    });
+    
+    it('Block form test',()=>{
+        cy.get('#inputFirstName[placeholder = "First Name"]').type('Muhammad Yusril').should('have.value','Muhammad Yusril')
+        cy.get('#inputLastName[placeholder = "Last Name"]').type('Hasriansyah').should('have.value','Hasriansyah')
+        cy.get('#inputEmail[placeholder = "Email"]').type('yusrilhasriansyah@gmail.com').should('have.value','yusrilhasriansyah@gmail.com')
+        cy.get('#inputWebsite[placeholder = "Website"]').type('https://github.com/myusrilh').should('have.value','https://github.com/myusrilh')
     });
 
 });
